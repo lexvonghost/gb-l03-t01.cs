b69b8872597d507c2fe2ad5729e6891e6e7aaf79
lexvonghost <lex_von_tost@mail.ru>
2022-07-27 23:08:34 +0300

    feat: решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 9999 && n <= 99999)
+{
+    string s = n.ToString();
+    char d1 = s[0];
+    char d2 = s[1];
+    char d3 = s[2];
+    char d4 = s[3];
+    char d5 = s[4];
+    if(d1 == d5 && d2 == d4)
+    {
+        Console.WriteLine("Число является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является полиндромом");
+    }
+
+}
+else
+{
+    Console.WriteLine("Неправильный ввод");
+}
